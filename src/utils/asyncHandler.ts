5385395c97e06c1c9e11144cf42a5c40bcfe9eb7
Harsh <hgaur491@gmail.com>
2024-06-18 20:25:19 +0530

    async handler

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-
+// asyncHandler: Use this if you want to directly handle errors and send a custom response without relying on centralized error handling.
 export const asyncHandler = (fn: any) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
@@ -10,5 +10,12 @@ export const asyncHandler = (fn: any) => {
                 message: error.message,
             });
         }
+    };
+};
+
+// asyncPromiseHandler: Use this if you prefer a centralized error handling approach and have middleware designed to handle errors globally.
+export const asyncPromiseHandler = (fn: any) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch((error: any) => next(error));
     };
 };
